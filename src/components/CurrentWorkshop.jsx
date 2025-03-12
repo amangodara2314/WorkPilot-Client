60f45d1491639d181f1ffb3ddcc1b4bc8ecba4d0
aman godara <amangodara5686@gmail.com>
2025-03-12 18:21:40 +0530

    designed the member page

--- src/components/CurrentWorkshop.jsx
+++ src/components/CurrentWorkshop.jsx
@@ -4,7 +4,7 @@ export default function CurrentWorkshop({ size = 8 }) {
   return (
     <div className="flex gap-4 items-center">
       <div
-        className={`flex aspect-square w-${size} h-${size} items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground`}
+        className={`flex aspect-square w-${size} h-${size} p-3 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground`}
       >
         <Settings size={size * 2} />
       </div>
--- src/pages/Members.jsx
+++ src/pages/Members.jsx
@@ -1,9 +1,65 @@
 import CurrentWorkshop from "@/components/CurrentWorkshop";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { Check, ClipboardCopy, CopyIcon } from "lucide-react";
+import { useState } from "react";
+import { toast } from "sonner";
 
 export default function Members() {
+  const inviteLink = "https://yourwebsite.com/invite/12345";
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(inviteLink).then(() => {
+      setCopied(true);
+      toast.success("Invite link copied!", {
+        style: { background: "#22c55e", color: "white" }, // Tailwind green-500
+      });
+      setTimeout(() => setCopied(false), 2000);
+    });
+  };
   return (
-    <div className="py-16 px-32">
-      <CurrentWorkshop size={16} />
+    <div className="flex justify-center">
+      <div className="flex-1 py-6 max-w-[75%] space-y-6">
+        <CurrentWorkshop size={16} />
+        <Separator className="" />
+
+        <div className="flex items-center justify-between">
+          <div>
+            <h1 className="text-lg font-semibold">Workshop Members</h1>
+            <p className="text-sm text-muted-foreground">
+              Workspace members can view and join all Workspace project, tasks
+              and create new task in the Workspace.{" "}
+            </p>
+          </div>
+        </div>
+        <Separator className="" />
+        <div className="flex items-center justify-between">
+          <div>
+            <h1 className="text-lg font-semibold">
+              Invite Members in your Workshop
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Anyone with an invite link can join this free Workspace. You can
+              also disable and create a new invite link for this Workspace at
+              any time.
+            </p>
+          </div>
+        </div>
+        <div className="flex items-center space-x-2 max-w-full">
+          <Input value={inviteLink} readOnly className="cursor-default" />
+          <Button
+            onClick={handleCopy}
+            variant="outline"
+            disabled={copied}
+            className="bg-[#18181B] text-white hover:text-white hover:bg-[#18181B]/90"
+          >
+            {!copied ? <CopyIcon /> : <Check />}
+          </Button>
+        </div>
+        <Separator className="" />
+      </div>
     </div>
   );
 }
